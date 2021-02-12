38b9fae7906ea7dcb6b5a455ae776b3df4777200
sfterh <sandeep.ertech@hotmail.com>
2021-02-12 18:25:34 +0530

    Update createvnet.bat

--- resources/createvnet.bat
+++ resources/createvnet.bat
@@ -1,3 +1,3 @@
 
-bat 'az login --service-principal -u ae850628-c022-4fef-9eb2-159a8ad5f743 -p JvFEy~r8Bvp3wG-DT5~Qg956t9hC49bZ7. -t 1bfc3093-d35c-42eb-8df1-47a59e098146'
-bat 'az network vnet create -g rg-west-prod-IAUCDR -n jenkinsVNET'
+az login --service-principal -u ae850628-c022-4fef-9eb2-159a8ad5f743 -p JvFEy~r8Bvp3wG-DT5~Qg956t9hC49bZ7. -t 1bfc3093-d35c-42eb-8df1-47a59e098146
+az network vnet create -g rg-west-prod-IAUCDR -n jenkinsVNET
