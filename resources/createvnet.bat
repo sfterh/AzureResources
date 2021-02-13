490d4df20a9b053e67c63c2e99851194c2578b49
sfterh <sandeep.ertech@hotmail.com>
2021-02-13 18:15:58 +0530

    Update createvnet.bat

--- resources/createvnet.bat
+++ resources/createvnet.bat
@@ -1,2 +1,3 @@
 
 CALL az login --service-principal -u ae850628-c022-4fef-9eb2-159a8ad5f743 -p JvFEy~r8Bvp3wG-DT5~Qg956t9hC49bZ7. -t 1bfc3093-d35c-42eb-8df1-47a59e098146
+CALL Get-AzResourceGroup -Name rg-east-Prod-IAUCDR
